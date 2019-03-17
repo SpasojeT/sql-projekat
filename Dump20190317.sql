0cf8c90e949e996782f6e18d73bd6339356760c2
Milos Petrovic <milospetrovic008@gmail.com>
2019-03-17 15:25:47 +0100

    Tables data and one procedure were added

--- Dump20190317.sql
+++ Dump20190317.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `survey_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `survey_db`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
--- Host: localhost    Database: survey_db
+-- Host: 127.0.0.1    Database: survey_db
 -- ------------------------------------------------------
 -- Server version	8.0.15
 
@@ -31,7 +29,7 @@ CREATE TABLE `answers` (
   PRIMARY KEY (`id`),
   KEY `FK3erw1a3t0r78st8ty27x6v3g1` (`question_id`),
   CONSTRAINT `FK3erw1a3t0r78st8ty27x6v3g1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +38,7 @@ CREATE TABLE `answers` (
 
 LOCK TABLES `answers` WRITE;
 /*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+INSERT INTO `answers` VALUES (1,'Answer about sausages',5),(2,'Answer about oranges',1),(3,'Second answer about oranges',1),(4,'Answer about lemon',2),(5,'Answer about football',3),(6,'Answer about basketball',4);
 /*!40000 ALTER TABLE `answers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +56,7 @@ CREATE TABLE `question_options` (
   PRIMARY KEY (`id`),
   KEY `FKsb9v00wdrgc9qojtjkv7e1gkp` (`question_id`),
   CONSTRAINT `FKsb9v00wdrgc9qojtjkv7e1gkp` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,6 +65,7 @@ CREATE TABLE `question_options` (
 
 LOCK TABLES `question_options` WRITE;
 /*!40000 ALTER TABLE `question_options` DISABLE KEYS */;
+INSERT INTO `question_options` VALUES (1,'Question option one',1),(2,'Question option two',2),(3,'Question option one',3),(4,'Question option two',4),(5,'Question option one',5);
 /*!40000 ALTER TABLE `question_options` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,7 +85,7 @@ CREATE TABLE `questions` (
   PRIMARY KEY (`id`),
   KEY `FKnf38uiy78c0g0tmo68btk3y0p` (`survey_id`),
   CONSTRAINT `FKnf38uiy78c0g0tmo68btk3y0p` FOREIGN KEY (`survey_id`) REFERENCES `surveys` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,6 +94,7 @@ CREATE TABLE `questions` (
 
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'First question','Oranges',1,1),(2,'Second question','Lemon',1,2),(3,'Third question','Sausages',1,3),(4,'Forth question','Football',2,4),(5,'Fifth question','Basketball',2,5),(7,'Sixth question','Football',2,3),(8,'Seventh question','Football',2,4);
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,7 +111,7 @@ CREATE TABLE `surveys` (
   `description` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,8 +120,32 @@ CREATE TABLE `surveys` (
 
 LOCK TABLES `surveys` WRITE;
 /*!40000 ALTER TABLE `surveys` DISABLE KEYS */;
+INSERT INTO `surveys` VALUES (1,'2013-05-12 14:10:10','Description about first survey','First survey'),(2,'2013-06-12 15:20:10','Description about second survey','Second survey'),(3,'2014-11-12 12:10:10','Description about third survey','Third survey'),(4,'2015-03-16 13:10:20','Description about forth survey','Forth survey'),(5,'2016-12-05 17:30:10','Description about fifth survey','Fifth survey');
 /*!40000 ALTER TABLE `surveys` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'survey_db'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `select_questions` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `select_questions`(IN num INT(3), IN title CHAR(10))
+BEGIN
+	SELECT * FROM questions WHERE questions.title = title LIMIT num;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -131,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-16 14:32:44
+-- Dump completed on 2019-03-17 15:23:29
